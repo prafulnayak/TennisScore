324fcc26375abac74e470843033ce935919b8821
Praful Nayak <prafulnayak1988@gmail.com>
2018-06-09 00:47:23 +0530

    Disable positive button if EditText is empty in AlertDialog with prompt layout.

--- app/src/main/java/org/sairaa/tennisscore/MainActivity.java
+++ app/src/main/java/org/sairaa/tennisscore/MainActivity.java
@@ -1,9 +1,13 @@
 package org.sairaa.tennisscore;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -46,7 +50,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         teaBreak = findViewById(R.id.teabreak);
         teaBreak.setOnClickListener(this);
 
+        // get prompt.xml view
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.prompt, null);
 
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = promptsView
+                .findViewById(R.id.editText_player1);
+
+        final EditText userInput2 = promptsView
+                .findViewById(R.id.editText_player2);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton(R.string.set_details,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // get user input and set it to result
+                                // edit text
+//                                player1.setText(userInput.getText());
+//                                player2.setText(userInput2.getText());
+//                                dialog.dismiss();
+
+
+                            }
+                        });
+
+
+
+        // create alert dialog
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Boolean wantToCloseDialog = (userInput.getText().toString().trim().isEmpty() && userInput2.getText().toString().trim().isEmpty());
+                // if EditText is empty disable closing on possitive button
+                if (!userInput.getText().toString().trim().isEmpty() && !userInput2.getText().toString().trim().isEmpty()){
+                    player1.setText(userInput.getText());
+                    player2.setText(userInput2.getText());
+                    alertDialog.dismiss();
+                }
+            }
+        });
+        alertDialog.getWindow().setBackgroundDrawableResource(R.color.colorBackground);
     }
 
     @Override
